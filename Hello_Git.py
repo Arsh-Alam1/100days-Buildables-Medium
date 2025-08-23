bad52d162d60835165d11115d97683ea0f3250c5
Arsh-Alam1 <arshalambhatti@gmail.com>
2025-08-23 23:47:58 +0500

    Added Hello_Git.py with Initial Code

---
+++ Hello_Git.py
@@ -0,0 +1,11 @@
+Name= "Arsh Alam"
+Hobbies =["sketching","Cooking"]
+Fav Top= ["ML & AI" , "Data Science" , "Game Deveploment"]
+print("My Name is :", Name)
+print("\nMy Favorite Hobbies:")
+for hobby in Hobbies:
+    print("-", hobby)
+
+print("\nMy Favorite Topics:")
+for topic in Fav Top:
+    print("-", topic)
